eafc27a79b5dccf1f3e73105b985952005634526
JANGDEOKSIK1 <wkdejrtlr@naver.com>
2020-06-26 15:30:41 +0900

    55

--- webpack.config.js
+++ webpack.config.js
@@ -8,5 +8,23 @@ module.exports = {
   output: {
     filename: '[name].js',
     path: path.resolve('./dist'),
+  },
+  module: {
+    rules: [    //rules라는 규칙 배열에 설정
+      {
+        test: /\.(jpg|png|gif|svg)$/,
+        loader: "url-loader",
+        options: {
+          name: "[name].[ext]?[hash]",
+          publicPath: "./dist/",
+          limit: 2000 
+        }
+      },
+      {
+        test: /\.css$/,
+        use:["style-loader", "css-loader"]
+      }
+    ]
   }
 }
+
